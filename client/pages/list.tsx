08b39c1ecf6ed0e356cfec38e1c97417acb9d0a1
okapie <tatigreuze@yahoo.co.jp>
2019-03-10 11:47:47 +0900

    Modify List component.

--- client/pages/list.tsx
+++ client/pages/list.tsx
@@ -5,32 +5,33 @@ import { Dispatch } from "redux";
 import modules from "../modules";
 import { CtxType, ListProps } from "../interfaces/pages"
 
-class List extends React.Component<ListProps, {}> {
-  static async getInitialProps(props: CtxType) {
-    const { store, isServer } = props.ctx;
-
-    // The following code is useful in case of null check for stored todos' list every time.
-    // if (!store.getState().todos.list) {
-    //   store.dispatch(modules.action.getTodosList());
-    // }
-
-    store.dispatch(modules.action.getTodosList());
-    return { isServer };
-  }
-
-  render() {
-    const { list } = this.props.todos;
-    return (
-      <div>
-        <ButtonComponent value='Information' onChange={() => {}} />
-        <input value={"Dummy"} onChange={() => {}} />
-        <ButtonComponent value='Add' onChange={() => {}} />
-        <ButtonComponent value='Show List' onChange={() => {}} />
-        <ul>{!list.isFetching && list.data.map(({ item }, index: number) => <li key={`item_${index}`}>{item}</li>)}</ul>
-      </div>
-    );
-  }
+const List = ({ todos }: ListProps) => {
+  const { list } = todos;
+  return (
+    <div>
+      <ButtonComponent value='Information' onChange={() => {}} />
+      <input value={"Dummy"} onChange={() => {}} />
+      <ButtonComponent value='Add' onChange={() => {}} />
+      <ButtonComponent value='Show List' onChange={() => {}} />
+      <ul>{!list.isFetching && list.data.map(({ item }, index: number) => <li key={`item_${index}`}>{item}</li>)}</ul>
+    </div>
+  );
 }
+
+List.getInitialProps = async (props: CtxType) => {
+  const { store, isServer } = props.ctx;
+
+  // The following code is useful in case of null check for stored todos' list every time.
+  // if (!store.getState().todos.list) {
+  //   store.dispatch(modules.action.getTodosList());
+  // }
+
+  store.dispatch(modules.action.getTodosList());
+
+  return {
+    isServer,
+  };
+};
 
 const mapDispatchToProps = (dispatch: Dispatch) => ({
   getTodosList: () => dispatch(modules.action.getTodosList())
